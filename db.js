1b216f65a55f25cad8d00822e8d07314eef3c83e
sssgordon <gso7@uwo.ca>
2019-11-08 11:51:23 +0100

    Set up docker and database in db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+const databaseURL =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
+
+const db = new Sequelize(databaseURL);
+
+db.sync({ force: false })
+  .then(() => console.log("Database connected"))
+  .catch(console.error);
+
+module.exports = db;
